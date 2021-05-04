4b28b86bd8a4e02261551ce432843b8d93968d0a
themarkwilliams <themarkwilliams@users.noreply.github.com>
2021-05-04 14:42:42 -0500

    Blocking sheet-overlay

--- EnhanceBizJournal.js
+++ EnhanceBizJournal.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Enhance BizJournal
-// @version 1.2
+// @version 1.3
 // @description "A user script to automatically bypass the paywall by marking the content as free."
 // @match https://*.bizjournals.com/*
 // @run-at document-start
@@ -12,3 +12,5 @@
 GM_addStyle('.pre-chunks { display:block !important; }'); // 5/8/18
 
 GM_addStyle('.js-pre-chunks__story-body { display:block !important; }'); // 7/14/18
+
+GM_addStyle('.sheet-overlay { display:block !important; }'); // 5/4/21
